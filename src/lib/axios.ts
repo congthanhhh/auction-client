a153ac7757238c6874232fa5e2874db512f2efcd
congthanhhh <congthanhh203@gmail.com>
2026-01-11 10:42:46 +0700

    update base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,7 @@ import axios, { AxiosError, type InternalAxiosRequestConfig } from 'axios';
 
 // 1. Khởi tạo instance
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
